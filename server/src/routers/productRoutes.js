b27ccb237d823eebf723e9ca5c554510fe63f5d7
Muhammad Zafeer <zafeerwains@gmail.com>
2023-11-17 15:45:44 +0500

    transactions

--- server/src/routers/productRoutes.js
+++ server/src/routers/productRoutes.js
@@ -30,46 +30,4 @@ router.post("/addProduct", async (req, res) => {
     res.json(studentsData);
   });
 
-  // Update Products
-
-  router.put("/updateProduct/:id", async (req, res) => {
-    try {
-      const productId = req.params.id;
-      const dataToBeUpdate = new Products({
-        name: req.body.name,
-        price: req.body.price,
-        category: req.body.category,
-        tags: req.body.tags,
-        specifications: req.body.specifications
-      });
-  
-      const updatedProduct = await Students.findByIdAndUpdate(productId, dataToBeUpdate, {
-        new: true,
-      });
-  
-      if (!updatedProduct) {
-        return res.status(404).json({ message: "Product not found!" });
-      }
-  
-      return res.json({ message: "Product updated successfuly!" });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
-  
-//Delete product
-
-  router.delete("/deleteProduct/:id", async (req, res) => {
-    try {
-      const productId = req.params.id;
-      const deleteProduct = await Products.findByIdAndRemove(productId);
-      if (!deleteProduct) {
-        return res.status(404).json({ message: "Product not found!" });
-      }
-  
-      return res.json({ message: "Product deleted successfuly!" });
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
-    }
-  });
   module.exports = router;
